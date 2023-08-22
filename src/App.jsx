815201c9bc730e50dc0f5d7f0a12ef51c20942c7
Martin Winther <martin.winther@protonmail.com>
2023-08-22 11:56:42 +0200

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "../pages/Home";
 import About from "../pages/About";
+
 import Vans from "../pages/Vans/Vans";
-import Layout from "../components/Layout";
 import VanDetail from "../pages/Vans/vanDetail";
+
 import Dashboard from "../pages/Host/Dashboard";
 import Income from "../pages/Host/Income";
 import Reviews from "../pages/Host/Reviews";
 
-import "./server";
+import Layout from "../components/Layout";
 import HostLayout from "../components/HostLayout";
+
+import "./server";
 
 function App() {
 	return (
