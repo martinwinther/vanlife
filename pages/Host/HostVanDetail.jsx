50cf4357317ce6059f0f4456d23d3791618f5ecb
Martin Winther <martin.winther@protonmail.com>
2023-09-02 08:35:39 +0200

    host fixes

--- pages/Host/HostVanDetail.jsx
+++ pages/Host/HostVanDetail.jsx
@@ -16,11 +16,19 @@ export default function HostVanDetail() {
 	}
 
 	return (
-		<div>
-			<img src={currentVan.imageUrl} width={150} />
-			<h2>{currentVan.name}</h2>
-			<p>{currentVan.price}</p>
-			<p>{currentVan.type}</p>
-		</div>
+		<section>
+			<div className="host-van-detail-layout-container">
+				<div className="host-van-detail">
+					<img src={currentVan.imageUrl} />
+					<div className="host-van-detail-info-text">
+						<i className={`van-type van-type-${currentVan.type}`}>
+							{currentVan.type}
+						</i>
+						<h3>{currentVan.name}</h3>
+						<h4>${currentVan.price}/day</h4>
+					</div>
+				</div>
+			</div>
+		</section>
 	);
 }
